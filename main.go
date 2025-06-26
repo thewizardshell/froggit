96915ac96480c99ddd2bd133e05b6664de7f6328
thewizardshell <vsroac@gmail.com>
2025-06-26 14:04:09 -0400

    feat: new version update

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	VERSION            = "beta-0.2.0"
+	VERSION            = "beta-0.2.1"
 	AUTHOR             = "Vicente Roa | Github: @thewizardshell"
 	REPO               = "https://github.com/thewizardshell/froggit"
 	SUPPORTED_COMMANDS = `
